1bcc3dc3788048a99a96b84dcd42823687c5c82a
Farmerin <infofarmerin@gmail.com>
2024-08-01 11:24:18 -0300

    MOTIVO BAJA NEW

--- pages/MOTIVODEBAJA.js
+++ pages/MOTIVODEBAJA.js
@@ -1,59 +1,101 @@
 import React, { useState, useContext } from "react";
 import { FirebaseContext } from '../firebase2';
 import Layout from '../components/layout/layout';
+import { parse, differenceInDays } from 'date-fns';
+import { Contenedor } from '../components/ui/Elementos';
 
-const MiComponente = () => {
-  const [animalesConBaja, setAnimalesConBaja] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const { firebase, tamboSel } = useContext(FirebaseContext);
+const MotivoBaja = () => {
+    const [animalesConBaja, setAnimalesConBaja] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const { firebase, tamboSel } = useContext(FirebaseContext);
 
-  // Función para obtener animales con mbaja y fbaja no vacíos
-  const obtenerAnimalesConBaja = async () => {
-    setLoading(true);
-    setError(null);
-    try {
-      // Referencia a la colección 'animal'
-      const querySnapshot = await firebase.db.collection('animal')
-        .where('idtambo', '==', tamboSel.id)
-        .get();
+    const diasLimite = 100;      // variable para comparar los dias
 
-      const animales = [];
-      querySnapshot.forEach((doc) => {
-        const data = doc.data();
-        // Verifica que mbaja y fbaja no estén vacíos
-        if (data.mbaja && data.fbaja) {
-          animales.push({ id: doc.id, ...data });
+    // Función para obtener animales con mbaja y fbaja no vacíos
+    const obtenerAnimalesConBaja = async () => {
+        setLoading(true);
+        setError(null);
+        try {
+            // Referencia a la colección 'animal'
+            const querySnapshot = await firebase.db.collection('animal')
+                .where('idtambo', '==', tamboSel.id)
+                .get();
+
+            const animales = [];
+            querySnapshot.forEach((doc) => {
+                const data = doc.data();
+                // Verifica que mbaja y fbaja no estén vacíos y que fbaja tenga más de 60 días
+                if (data.mbaja && data.fbaja) {
+                    const fbaja = parse(data.fbaja, 'yyyy-MM-dd', new Date());
+                    const diasDesdeBaja = differenceInDays(new Date(), fbaja);
+                    if (diasDesdeBaja < diasLimite) {
+                        animales.push({ id: doc.id, ...data });
+                    }
+                }
+            });
+
+            setAnimalesConBaja(animales);
+        } catch (error) {
+            console.error("Error obteniendo animales:", error);
+            setError("Error obteniendo animales. Por favor, intenta de nuevo.");
+        } finally {
+            setLoading(false);
         }
-      });
+    };
 
-      setAnimalesConBaja(animales);
-    } catch (error) {
-      console.error("Error obteniendo animales:", error);
-      setError("Error obteniendo animales. Por favor, intenta de nuevo.");
-    } finally {
-      setLoading(false);
-    }
-  };
+    const eliminarTodosLosAnimalesConBaja = async () => {
+        try {
+            const batch = firebase.db.batch();
 
-  return (
-    <Layout titulo="Herramientas">
-      <div>
-        <button onClick={obtenerAnimalesConBaja} disabled={loading}>
-          {loading ? "Cargando..." : "Obtener Animales con Baja"}
-        </button>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-        <ul>
-          {animalesConBaja.length === 0 && !loading && <li>No se encontraron animales con baja.</li>}
-          {animalesConBaja.map((animal) => (
-            <li key={animal.id}>
-              Nombre: {animal.rp}, ID: {animal.erp}, mbaja: {animal.mbaja}, fbaja: {animal.fbaja}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </Layout>
-  );
+            animalesConBaja.forEach((animal) => {
+                const docRef = firebase.db.collection('animal').doc(animal.id);
+                batch.delete(docRef);
+            });
+
+            await batch.commit();
+            console.log("Todos los animales con baja eliminados exitosamente");
+
+            // Limpiar la lista de animales con baja
+            setAnimalesConBaja([]);
+        } catch (error) {
+            console.error("Error eliminando animales:", error);
+            setError("Error eliminando animales. Por favor, intenta de nuevo.");
+        }
+    };
+
+    const manejarEliminarTodosLosAnimales = async () => {
+        await obtenerAnimalesConBaja();
+        eliminarTodosLosAnimalesConBaja();
+    };
+
+
+    return (
+        <Layout titulo="Obtener y Eliminar">
+            <div className="motivoBaja">
+                <button className="botObtener" onClick={obtenerAnimalesConBaja} disabled={loading}>
+                    {loading ? "Cargando..." : "Obtener Animales con Baja"}
+                </button>
+
+                <button className="botEliminar" onClick={manejarEliminarTodosLosAnimales}>
+                    {loading ? "Cargando..." : "Eliminar todos los animales con baja"}
+                </button>
+            </div>
+            <Contenedor>
+                <h3>Listado de animales con motivo de baja:</h3>
+                <h4>cantidad de animales: {animalesConBaja.length}</h4>
+                {error && <p style={{ color: 'red' }}>{error}</p>}
+                <ul>
+                    {animalesConBaja.length === 0 && !loading && <li>No se encontraron animales con baja.</li>}
+                    {animalesConBaja.map((animal) => (
+                        <li key={animal.id}>
+                            Nombre: {animal.rp}, eRP: {animal.erp}, mbaja: {animal.mbaja}, fbaja: {animal.fbaja}
+                        </li>
+                    ))}
+                </ul>
+            </Contenedor>
+        </Layout>
+    );
 };
 
-export default MiComponente;
+export default MotivoBaja;
--- pages/parametros.js
+++ pages/parametros.js
@@ -6,7 +6,6 @@ import Layout from '../components/layout/layout';
 import DetalleParametro from '../components/layout/detalleParametro';
 import SelectTambo from '../components/layout/selectTambo';
 import { Button, DropdownButton, Dropdown } from 'react-bootstrap';
-import { format } from 'date-fns';
 import { addNotification } from '../redux/notificacionSlice';
 
 const Parametros = () => {
@@ -50,38 +49,42 @@ const Parametros = () => {
     setPorc(nuevoPorcentaje);
 
     try {
-      await firebase.db.collection('tambo').doc(tamboSel.id).update(p);
+        // Actualizar porcentaje en la colección 'tambo'
+        await firebase.db.collection('tambo').doc(tamboSel.id).update(p);
 
-      // Obtener la colección de animales
-      const animalesSnapshot = await firebase.db.collection('animal').where('tamboId', '==', tamboSel.id).get();
+        // Obtener la colección de animales y filtrar documentos
+        const animalesSnapshot = await firebase.db.collection('animal')
+            .where('tamboId', '==', tamboSel.id)
+            .get();
 
-      animalesSnapshot.forEach(async (doc) => {
-        const animalData = doc.data();
-        if (!animalData.fbaja && !animalData.mbaja) {
-          await firebase.db.collection('animal').doc(doc.id).update(pAnimal);
-        }
-      });
+        animalesSnapshot.forEach(async (doc) => {
+            const docData = doc.data();
+            if (docData.fbaja === '' && docData.mbaja === '') {
+                const docRef = firebase.db.collection('animal').doc(doc.id);
+                await docRef.update(pAnimal);
+            }
+        });
 
-      // Agregar notificación en Firestore
-      await firebase.db.collection('tambo').doc(tamboSel.id).collection('notificaciones').add({
-        mensaje: isIncrease ? `AUMENTO DEL ${selectedChange}%` : `REDUCCIÓN DEL ${selectedChange}%`,
-        fecha:  firebase.nowTimeStamp(),
-      });
+        // Agregar notificación en Firestore
+        await firebase.db.collection('tambo').doc(tamboSel.id).collection('notificaciones').add({
+            mensaje: isIncrease ? `AUMENTO DEL ${selectedChange}%` : `REDUCCIÓN DEL ${selectedChange}%`,
+            fecha: firebase.nowTimeStamp(),
+        });
 
-      // Agregar notificación en Redux
-      dispatch(addNotification({
-        id: Date.now(),
-        mensaje: isIncrease ? `AUMENTO DEL ${selectedChange}%` : `REDUCCIÓN DEL ${selectedChange}%`,
-        fecha:  firebase.nowTimeStamp(),
-      }));
+        // Agregar notificación en Redux
+        dispatch(addNotification({
+            id: Date.now(),
+            mensaje: isIncrease ? `AUMENTO DEL ${selectedChange}%` : `REDUCCIÓN DEL ${selectedChange}%`,
+            fecha: firebase.nowTimeStamp(),
+        }));
 
-      console.log(tamboSel);
+        console.log(tamboSel);
     } catch (error) {
-      console.log(error);
+        console.log(error);
     }
     setValor(nuevoPorcentaje);
     setSelectedChange(null);
-  };
+};
 
   const restablecer = async () => {
     if (tamboSel) {
@@ -93,14 +96,16 @@ const Parametros = () => {
         // Actualizar el porcentaje en la colección 'tambo'
         await firebase.db.collection('tambo').doc(tamboSel.id).update(p);
 
-        // Obtener la colección de animales
-        const animalesSnapshot = await firebase.db.collection('animal').where('tamboId', '==', tamboSel.id).get();
+        // Obtener la colección de animales y filtrar documentos
+        const animalesSnapshot = await firebase.db.collection('animal')
+          .where('tamboId', '==', tamboSel.id)
+          .where('fbaja', '==', '')  // Filtra animales sin fbaja
+          .where('mbaja', '==', '')  // Filtra animales sin mbaja
+          .get();
 
         animalesSnapshot.forEach(async (doc) => {
-          const animalData = doc.data();
-          if (!animalData.fbaja && !animalData.mbaja) {
-            await firebase.db.collection('animal').doc(doc.id).update(pAnimal);
-          }
+          const docRef = firebase.db.collection('animal').doc(doc.id);
+          await docRef.update(pAnimal);
         });
 
         // Agregar notificación en Firestore
@@ -113,7 +118,7 @@ const Parametros = () => {
         dispatch(addNotification({
           id: Date.now(),
           mensaje: 'SE VOLVIÓ AL VALOR ORIGINAL DE LA RACIÓN.',
-          fecha:  firebase.nowTimeStamp(),
+          fecha: firebase.nowTimeStamp(),
         }));
 
         console.log('se ejecutó y se agregó la notificación');
@@ -144,86 +149,85 @@ const Parametros = () => {
 
   return (
     <Layout titulo="Parámetros Nutricionales">
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-      <h1 style={{ color: "#404040", fontWeight: "bold", fontSize: "34px", marginTop: 0 }}>ALIMENTACIÓN</h1>
-      <div style={{ display: "flex", flexDirection: "row", gap: "7px" }}>
-        <p style={{ color: "grey", fontWeight: "bold", fontSize: "18px", marginTop: 0 }}>ESTADO ACTUAL: </p>
-        <p style={{ color: "#404040", fontWeight: "bold", fontSize: "18px" }}>
-          {valor === 0 ? "POR DEFECTO" : null}
-          {valor < 0 ? "REDUCCIÓN DEL " + valor + "%" : null}
-          {valor > 0 ? "AUMENTO DEL " + valor + "%" : null}
-        </p>
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+        <h1 style={{ color: "#404040", fontWeight: "bold", fontSize: "34px", marginTop: 0 }}>ALIMENTACIÓN</h1>
+        <div style={{ display: "flex", flexDirection: "row", gap: "7px" }}>
+          <p style={{ color: "grey", fontWeight: "bold", fontSize: "18px", marginTop: 0 }}>ESTADO ACTUAL: </p>
+          <p style={{ color: "#404040", fontWeight: "bold", fontSize: "18px" }}>
+            {valor === 0 ? "POR DEFECTO" : null}
+            {valor < 0 ? "REDUCCIÓN DEL " + valor + "%" : null}
+            {valor > 0 ? "AUMENTO DEL " + valor + "%" : null}
+          </p>
+        </div>
       </div>
-    </div>
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", gap: "10px" }}>
-      <div style={{ display: "flex", flexDirection: "row", gap: "10px" }}>
-        <DropdownButton
-          id="dropdown-reducir-button"
-          title={isIncrease === false && selectedChange !== null ? `Reducción: ${selectedChange}%` : 'Seleccionar Reducción'}
-          variant="danger"
-          className='dropdown-reducir-button'
-          onSelect={(e) => { setSelectedChange(parseInt(e)); setIsIncrease(false); }}
-        >
-          <Dropdown.Item eventKey="-10">-10%</Dropdown.Item>
-          <Dropdown.Item eventKey="-20">-20%</Dropdown.Item>
-          <Dropdown.Item eventKey="-30">-30%</Dropdown.Item>
-          <Dropdown.Item eventKey="-40">-40%</Dropdown.Item>
-          <Dropdown.Item eventKey="-50">-50%</Dropdown.Item>
-        </DropdownButton>
-        <Button
-          style={{ fontWeight: "bold", borderRadius: "10px", width: "20%", height: "50px", fontSize: "18px", display: "flex", alignItems: "center", justifyContent: "center" }}
-          variant="info"
-          block
-          className='botonRestablecer'
-          onClick={restablecer}
-        >Restablecer</Button>
-        <DropdownButton
-          id="dropdown-aumentar-button"
-          title={isIncrease === true && selectedChange !== null ? `Aumento: ${selectedChange}%` : 'Seleccionar Aumento'}
-          variant="success"
-          className='dropdown-aumentar-button'
-          onSelect={(e) => { setSelectedChange(parseInt(e)); setIsIncrease(true); }}
-        >
-          <Dropdown.Item eventKey="10">10%</Dropdown.Item>
-          <Dropdown.Item eventKey="20">20%</Dropdown.Item>
-          <Dropdown.Item eventKey="30">30%</Dropdown.Item>
-          <Dropdown.Item eventKey="40">40%</Dropdown.Item>
-          <Dropdown.Item eventKey="50">50%</Dropdown.Item>
-          <Dropdown.Item eventKey="60">60%</Dropdown.Item>
-          <Dropdown.Item eventKey="70">70%</Dropdown.Item>
-          <Dropdown.Item eventKey="80">80%</Dropdown.Item>
-          <Dropdown.Item eventKey="90">90%</Dropdown.Item>
-          <Dropdown.Item eventKey="100">100%</Dropdown.Item>
-        </DropdownButton>
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", gap: "10px" }}>
+        <div style={{ display: "flex", flexDirection: "row", gap: "10px" }}>
+          <DropdownButton
+            id="dropdown-reducir-button"
+            title={isIncrease === false && selectedChange !== null ? `Reducción: ${selectedChange}%` : 'Seleccionar Reducción'}
+            variant="danger"
+            className='dropdown-reducir-button'
+            onSelect={(e) => { setSelectedChange(parseInt(e)); setIsIncrease(false); }}
+          >
+            <Dropdown.Item eventKey="-10">-10%</Dropdown.Item>
+            <Dropdown.Item eventKey="-20">-20%</Dropdown.Item>
+            <Dropdown.Item eventKey="-30">-30%</Dropdown.Item>
+            <Dropdown.Item eventKey="-40">-40%</Dropdown.Item>
+            <Dropdown.Item eventKey="-50">-50%</Dropdown.Item>
+          </DropdownButton>
+          <Button
+            style={{ fontWeight: "bold", borderRadius: "10px", width: "20%", height: "50px", fontSize: "18px", display: "flex", alignItems: "center", justifyContent: "center" }}
+            variant="info"
+            block
+            className='botonRestablecer'
+            onClick={restablecer}
+          >Restablecer</Button>
+          <DropdownButton
+            id="dropdown-aumentar-button"
+            title={isIncrease === true && selectedChange !== null ? `Aumento: ${selectedChange}%` : 'Seleccionar Aumento'}
+            variant="success"
+            className='dropdown-aumentar-button'
+            onSelect={(e) => { setSelectedChange(parseInt(e)); setIsIncrease(true); }}
+          >
+            <Dropdown.Item eventKey="10">10%</Dropdown.Item>
+            <Dropdown.Item eventKey="20">20%</Dropdown.Item>
+            <Dropdown.Item eventKey="30">30%</Dropdown.Item>
+            <Dropdown.Item eventKey="40">40%</Dropdown.Item>
+            <Dropdown.Item eventKey="50">50%</Dropdown.Item>
+            <Dropdown.Item eventKey="60">60%</Dropdown.Item>
+            <Dropdown.Item eventKey="70">70%</Dropdown.Item>
+            <Dropdown.Item eventKey="80">80%</Dropdown.Item>
+            <Dropdown.Item eventKey="90">90%</Dropdown.Item>
+            <Dropdown.Item eventKey="100">100%</Dropdown.Item>
+          </DropdownButton>
+        </div>
+        {selectedChange !== null && (
+          <Button
+            style={{ fontWeight: "bold", borderRadius: "10px", width: "20%", height: "50px", fontSize: "18px", display: "flex", alignItems: "center", justifyContent: "center"}}
+            variant="primary"
+            block
+            className='botonAplicar'
+            onClick={handleApplyChange}
+          >Aplicar</Button>
+        )}
       </div>
-      {selectedChange !== null && (
-        <Button
-          style={{ fontWeight: "bold", borderRadius: "10px", width: "20%", height: "50px", fontSize: "18px", display: "flex", alignItems: "center", justifyContent: "center"}}
-          variant="primary"
-          block
-          className='botonAplicar'
-          onClick={handleApplyChange}
-        >Aplicar</Button>
-      )}
-    </div>
-    {tamboSel ?
-      <>
-        <DetalleParametro
-          idTambo={tamboSel.id}
-          categoria="Vaquillona"
-          porcentaje={porcentaje}
-        />
-        <DetalleParametro
-          idTambo={tamboSel.id}
-          categoria="Vaca"
-          porcentaje={porcentaje}
-        />
-      </>
-      :
-      <SelectTambo />
-    }
-  </Layout>
-  
+      {tamboSel ?
+        <>
+          <DetalleParametro
+            idTambo={tamboSel.id}
+            categoria="Vaquillona"
+            porcentaje={porcentaje}
+          />
+          <DetalleParametro
+            idTambo={tamboSel.id}
+            categoria="Vaca"
+            porcentaje={porcentaje}
+          />
+        </>
+        :
+        <SelectTambo />
+      }
+    </Layout>
   );
 };
 
